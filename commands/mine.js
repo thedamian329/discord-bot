a8cab164eda5c2410bd5f6584e8062c28af2a6a9
thedamian329 <thedamian329@hotmail.com>
2024-11-13 18:57:30 -0500

    mine percentage change

--- commands/mine.js
+++ commands/mine.js
@@ -13,7 +13,7 @@ function mine(message, command, db, handleLevelUp) {
           );
         }
 
-        const goldChance = Math.random() < 0.1; // try fucking around with these numbers.
+        const goldChance = Math.random() < 0.7; // try fucking around with these numbers.
         const goldEarned = goldChance ? Math.floor(Math.random() * 100) + 1 : 0;
 
         db.run(
